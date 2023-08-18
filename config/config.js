d294869fe4aab4d41dce73e47099dace3154c19e
Ehsan Gazar <me@ehsangazar.com>
2023-08-19 00:57:49 +1000

    fix postgres data

--- config/config.js
+++ config/config.js
@@ -1,29 +1,21 @@
-// const path = require("path");
+const path = require("path");
 
-// let variablePath = "";
-// if (process.env.NODE_ENV === "production") {
-//   variablePath = path.join(__dirname, "../variables-production.env");
-// } else {
-//   variablePath = path.join(__dirname, "../variables.env");
-// }
-// require("dotenv").config({ path: variablePath });
+let variablePath = "";
+if (process.env.NODE_ENV === "production") {
+  variablePath = path.join(__dirname, "../variables-production.env");
+} else {
+  variablePath = path.join(__dirname, "../variables.env");
+}
+require("dotenv").config({ path: variablePath });
 
-// module.exports = {
-//   database: process.env.POSTGRES_DB_DATABSE,
-//   username: process.env.POSTGRES_DB_USER,
-//   password: process.env.POSTGRES_DB_PASSWORD,
-//   dialect: "postgres",
-//   host: process.env.POSTGRES_DB_HOST,
-//   port: process.env.POSTGRES_DB_PORT,
-//   use_env_variable: "DATABASE_URL",
-// };
-
-const config = {
-  username: "postgres",
-  password: "5O47ntONhuZpRTD",
-  database: "devmentor",
-  host: "0.0.0.0",
+module.exports = {
+  database: process.env.POSTGRES_DB_DATABSE,
+  username: process.env.POSTGRES_DB_USER,
+  password: process.env.POSTGRES_DB_PASSWORD,
   dialect: "postgres",
+  host: process.env.POSTGRES_DB_HOST,
+  port: process.env.POSTGRES_DB_PORT,
+  use_env_variable: "DATABASE_URL",
 };
 
 module.exports = config;
